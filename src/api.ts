e0998fce376730011c340714be2c0e941b5df091
Nil Portugués Calderó <nilopc@gmail.com>
2024-08-02 17:57:58 +0200

    quotes endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import quotes from './quotes';
 
 export const app = express();
 
@@ -16,9 +17,38 @@ app.get('/', (req, res) => {
 
 const api = express.Router();
 
+// Version the api
+app.use('/api/v1', api);
+
 api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
 });
 
-// Version the api
-app.use('/api/v1', api);
+
+// Function to paginate the quotes array
+function paginate(array, page, pageSize) {
+  const offset = (page - 1) * pageSize;
+  const paginatedItems = array.slice(offset, offset + pageSize);
+  const totalPages = Math.ceil(array.length / pageSize);
+  return {
+    page,
+    pageSize,
+    totalPages,
+    totalItems: array.length,
+    data: paginatedItems
+  };
+}
+
+// Route to get paginated quotes
+api.get('/quotes', (req: any, res) => {
+  const page = Number.parseInt(req.query.page) || 1;
+  const pageSize = Number.parseInt(req.query.pageSize) || 10;
+
+  if (page < 1 || pageSize < 1) {
+    res.status(400).json({ message: 'Page and pageSize must be positive integers.' });
+    return;
+  }
+
+  const paginatedQuotes = paginate(quotes, page, pageSize);
+  res.status(200).json(paginatedQuotes);
+});
